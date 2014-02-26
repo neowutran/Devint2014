a410e84dd4252dc377d2f976d68c734fbda6216a
Romain Guillot <rom1of0674@gmail.com>
2014-02-26 16:29:05 +0100

    webworker example

---
+++ Project/js/webworker example/main.js
@@ -0,0 +1,17 @@
+//Création d'une zone d'affichage
+var elem=document.createElement("output");
+elem.value = "Chargement du jeu ...;
+document.body.appendChild(elem);
+
+//Création d'un worker
+if(window.Worker){
+//le navigateur supporte les workers
+    var worker=new Worker("./worker.js");
+    worker.onmessage=function(event){
+        elem.value = event.data;
+    };
+}else{
+//le navigateur ne supporte pas les workers
+    alert("Désolé votre navigateur "+
+        "ne supporte pas les workers ! ☹");
+}
