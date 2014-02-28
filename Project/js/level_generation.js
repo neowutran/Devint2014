237d0cd0666eede7b3d82c5bda8d1991493d20f3
Aurelien COLOMBET <colombet.aurelien@etu.unice.fr>
2014-02-28 18:26:03 +0100

    ajout de id pour la generation de level

--- Project/js/level_generation.js
+++ Project/js/level_generation.js
@@ -5,6 +5,7 @@ function generation_level(time) {
     var high = 0;
     var pattern = [];
     var i = 0;
+	var cmp = 0;
 
     while (i < time) {								//put an obstacle all the 5secs
         var rand = Math.floor((Math.random() * 2) + 1); // Random between 1 and 2 .
@@ -18,6 +19,7 @@ function generation_level(time) {
             }
         }
         var item = {
+			"id": cmp,
             "input": rand,				//1 -> down  2-> up.
             "high": high,				//the high of the player (for a graphical game).
             "time": i					//time when the obstacle pop.
@@ -28,7 +30,7 @@ function generation_level(time) {
         var delay = computDelay(0, 1);
 
         i = i + delay;					//TODO maybe a better random.
-
+		cmp++;
 
     }
 
