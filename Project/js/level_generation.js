5402da9d5e0ead5631dd6cb4fc5014484f055cc8
Aurelien COLOMBET <colombet.aurelien@etu.unice.fr>
2014-03-02 18:34:09 +0100

    generation de level mis à jour pour renvoyer un JSON selon le sample fait par didier

--- Project/js/level_generation.js
+++ Project/js/level_generation.js
@@ -1,47 +1,78 @@
 'use strict';
 
-function generation_level(time) {
-    /*INITIALISATION */
-    var high = 0;
-    var pattern = [];
-    var i = 0;
-	var cmp = 0;
+			function generation_level(time){
+				/*INITIALISATION */
+				$('p').css('color', 'pink');			//TEST Jquery offline.
+				var u = 0;
+				var stringtmp;
+				var time;
+				var distance = 60;
+				var patern = [];
+				var JSONlevel = "";
+				var maxiTime = time;
+				var i=0;
+				var id = 0;
+				var cmp=0;
+				var j;
+				var rand=Math.floor((Math.random()*4)+1); // Random between 1 and 4 .
+			
+				while(i<=maxiTime){								
 
-    while (i < time) {								//put an obstacle all the 5secs
-        var rand = Math.floor((Math.random() * 2) + 1); // Random between 1 and 2 .
-        if (rand == 2) {
-            if (high <= 4) {			//the maximum high for a player is 4. can be change.
-                high++;
-            }
-        } else {
-            if (high != 0) {
-                high--;
-            }
-        }
-        var item = {
-			"id": cmp,
-            "input": rand,				//1 -> down  2-> up.
-            "high": high,				//the high of the player (for a graphical game).
-            "time": i					//time when the obstacle pop.
-        };
+					for(j=0;j<40;j++){						//40 frames per secondes.
+						
+						/*if(rand==2){
+							if(high<=4){			//the maximum high for a player is 4. can be change.
+								high++;
+							}
+						}
+						else{
+							if(high!=0){
+								high--;
+							}
+						}*/
+							var item = {
+								"distance" : distance,
+								"direction": rand,				//1 -> down  2-> up.
+								"id" : id					//time when the obstacle pop.
+							};
+					
+						patern.push(item);
+						//level.push(patern);
+						distance --;
+						if(distance < 0){
+							distance = computDelay(40,160);
+							id++;
+							rand=Math.floor((Math.random()*4)+1); // Random between 1 and 4 .
+						}
 
-        pattern.push(item);
+						if(i>maxiTime){
+							stringtmp='"'+JSON.stringify(cmp)+'"'+":"+JSON.stringify(patern);
+						}
+						else{
+							stringtmp='"'+JSON.stringify(cmp)+'"'+":"+JSON.stringify(patern)+",";
+						}
+						JSONlevel = JSONlevel+stringtmp;			//JSONlevel+JSON.stringify(patern)+","; //JSON.stringify({test: patern})
+						var patern =[];
+						cmp++;
+					}
 
-        var delay = computDelay(0, 1);
-
-        i = i + delay;					//TODO maybe a better random.
-		cmp++;
-
-    }
-
-    return JSON.stringify({level: pattern});
+					i++;					//TODO maybe a better random.
 
 
-}
+				}
+			
+				
+				JSONlevel = "{"+JSONlevel+"}";
+				//alert(myJSON);
+				return JSONlevel;
+				//return level;
 
-function computDelay(min, max) {
 
-    return Math.floor((Math.random() * (max - min)) + 1);
+			}
 
-}
+		function computDelay(min,max){
+
+			return Math.floor((Math.random()*(max-min))+1);
+
+		}
 
