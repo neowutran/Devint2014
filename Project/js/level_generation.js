8f844081dba5cd109665b5e08d243dd62fb7818a
Aurelien COLOMBET <colombet.aurelien@etu.unice.fr>
2014-03-21 14:01:29 +0100

    generation de level improvement

--- Project/js/level_generation.js
+++ Project/js/level_generation.js
@@ -18,6 +18,7 @@ function generation_level(time) {
         cmp = 0,
         j,
         rand = Math.floor((Math.random() * 4) + 1),
+        portee,
         item;
         //configuration;    //TODO pour le fichier de config.
 		/*$.getJSON("config.json", function(data ){
@@ -29,8 +30,16 @@ function generation_level(time) {
 
         for (j = 0; j < 40; j++) {						//40 frames per secondes.
 
+            if(distance<=0){
+                portee=0;
+            }
+            else{
+                portee=1;
+            }
+
+
             item = {
-                "distance": distance,
+                "distance": portee,
                 "direction": rand,				//1 -> down  2-> up.
                 "id": id					//time when the obstacle pop.
             };
@@ -43,18 +52,22 @@ function generation_level(time) {
                 rand = Math.floor((Math.random() * 4) + 1); // Random between 1 and 4 .
             }
 
+            if(distance===0){
             if (i >= time) {
-                stringtmp = '"' + JSON.stringify(cmp) + '"' + ":" + JSON.stringify(pattern);
+               stringtmp = '"' + JSON.stringify(cmp) + '"' + ":" + JSON.stringify(pattern);
             }
             else {
-                stringtmp = "";
-                if(JSONlevel !== ""){
-                    stringtmp = ",";
-                }
-                stringtmp += '"' + JSON.stringify(cmp) + '"' + ":" + JSON.stringify(pattern);
+               stringtmp = "";
+               if(JSONlevel !== ""){
+                   stringtmp = ",";
+               }
+               stringtmp += '"' + JSON.stringify(cmp) + '"' + ":" + JSON.stringify(pattern);
             }
+
             JSONlevel = JSONlevel + stringtmp;
+            }
             pattern = [];
+
             cmp++;
         }
 
@@ -65,8 +78,7 @@ function generation_level(time) {
 
 
     JSONlevel = "{" + JSONlevel + "}";
+    console.log(JSONlevel);
     return JSONlevel;
 
 }
-
-
