3960cad2390097e65c0ec18b55bef1fcaab2e583
Didier Martini <martini.didier1@etu.unice.fr>
2014-04-07 16:40:56 +0200

    micro

--- Project/js/micro/recorder.js
+++ Project/js/micro/recorder.js
@@ -1,110 +1,110 @@
 /*License (MIT)
 
-Copyright © 2013 Matt Diamond
+ Copyright © 2013 Matt Diamond
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
-to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-the Software.
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+ */
+var Recorder = function (source, cfg) {
+    'use strict';
 
-(function(window){
-  'use strict';
+    var WORKER_PATH = 'recorderWorker.js',
+        config = cfg || {},
+        bufferLen = config.bufferLen || 4096,
+        recording = false,
+        currCallback,
+        worker;
 
-  var WORKER_PATH = 'recorderWorker.js',
-      Recorder = function(source, cfg){
-    var config = cfg || {};
-    var bufferLen = config.bufferLen || 4096;
     this.context = source.context;
-    if(!this.context.createScriptProcessor){
-       this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
+    if (!this.context.createScriptProcessor) {
+        this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
     } else {
-       this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
+        this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
     }
 
-    var worker = new Worker(config.workerPath || WORKER_PATH);
+    worker = new Worker(WORKER_PATH);
     worker.postMessage({
-      command: 'init',
-      config: {
-        sampleRate: this.context.sampleRate
-      }
+        command: 'init',
+        config: {
+            sampleRate: this.context.sampleRate
+        }
     });
-    var recording = false,
-      currCallback;
 
-    this.node.onaudioprocess = function(e){
-      if (!recording){
-          return;
-      }
-      worker.postMessage({
-        command: 'record',
-        buffer: [
-          e.inputBuffer.getChannelData(0),
-          e.inputBuffer.getChannelData(1)
-        ]
-      });
+    this.node.onaudioprocess = function (e) {
+        if (!recording) {
+            return;
+        }
+        worker.postMessage({
+            command: 'record',
+            buffer: [
+                e.inputBuffer.getChannelData(0),
+                e.inputBuffer.getChannelData(1)
+            ]
+        });
     };
 
-    this.record = function(){
-      recording = true;
+    this.record = function () {
+        recording = true;
     };
 
-    this.stop = function(){
-      recording = false;
+    this.stop = function () {
+        recording = false;
     };
 
-    this.clear = function(){
-      worker.postMessage({ command: 'clear' });
+    this.clear = function () {
+        worker.postMessage({ command: 'clear' });
     };
 
-    this.getBuffers = function(cb) {
-      currCallback = cb || config.callback;
-      worker.postMessage({ command: 'getBuffers' });
+    this.getBuffers = function (cb) {
+        currCallback = cb || config.callback;
+        worker.postMessage({ command: 'getBuffers' });
     };
 
-    this.exportWAV = function(cb, type){
-      currCallback = cb || config.callback;
-      type = type || config.type || 'audio/wav';
-      if (!currCallback) throw new Error('Callback not set');
-      worker.postMessage({
-        command: 'exportWAV',
-        type: type
-      });
+    this.exportWAV = function (cb, type) {
+        currCallback = cb || config.callback;
+        type = type || config.type || 'audio/wav';
+        if (!currCallback) {
+            throw new Error('Callback not set');
+        }
+        worker.postMessage({
+            command: 'exportWAV',
+            type: type
+        });
     };
 
-    worker.onmessage = function(e){
-      var blob = e.data;
-      currCallback(blob);
+    worker.onmessage = function (e) {
+        var blob = e.data;
+        currCallback(blob);
     };
 
     source.connect(this.node);
     this.node.connect(this.context.destination);   // if the script node is not connected to an output the "onaudioprocess" event is not triggered in chrome.
-  };
-
-  Recorder.setupDownload = function(blob, filename){
-    var url = (window.URL || window.webkitURL).createObjectURL(blob);
-    var link = document.getElementById("save");
-    link.href = url;
-    link.download = filename || 'output.wav';
-  };
-
-  Recorder.saveToFile = function(blob, filename){
-      var url = (window.URL || window.webkitURL).createObjectURL(blob);
-      var link = document.getElementById("save");
-      link.href = url;
-      link.download = filename || 'output.wav';
-  };
 
 
-  window.Recorder = Recorder;
+    this.setupDownload = function (blob, filename) {
+        var url = (window.URL || window.webkitURL).createObjectURL(blob);
+        var link = document.getElementById("save");
+        link.href = url;
+        link.download = filename || 'output.wav';
+    };
 
-}(window));
+    this.saveToFile = function (blob, filename) {
+        var url = (window.URL || window.webkitURL).createObjectURL(blob);
+        var link = document.getElementById("save");
+        link.href = url;
+        link.download = filename || 'output.wav';
+    };
+
+};
+var recorder = new Recorder(window);
