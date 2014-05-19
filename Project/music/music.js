dd6af77871d80f46d2aabacfb7d552dc6eb6910c
Romain Guillot <rom1of0674@gmail.com>
2014-05-19 23:25:00 +0200

    beug fiwx

--- Project/music/music.js
+++ Project/music/music.js
@@ -1,9 +1,4 @@
 var music_list = [
-	 {"file":"olive_et_tom.ogg"} , 
-{"file":"pokemon_generique.mp3"} ,
-{"file":"simpsons.mp3"} ,
-{"file":"tetris.ogg"} 
-{"file":"remix_cuir_moustache.mp3"} 
-	{"file":"test/InShape_-_Engine.ogg"}
+	 {"file":"olive_et_tom.ogg"} , {"file":"pokemon_generique.mp3"} , {"file":"simpsons.mp3"} , {"file":"tetris.ogg"} , {"file":"remix_cuir_moustache.mp3"}, {"file":"test/InShape_-_Engine.ogg"}
 
 ];
