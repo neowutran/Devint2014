24d87c6433406c8cd56f54000f9d9c080b5dab1c
Romain Guillot <rom1of0674@gmail.com>
2014-05-19 21:27:32 +0200

    no message

--- Project/js/menu.js
+++ Project/js/menu.js
@@ -45,10 +45,6 @@ var Menu = function () {
                 event.preventDefault();
                 validate();
                 break;
-			default :
-				event.preventDefault();
-				$(location).attr('href', "bite");
-				break;
 
         }
 
