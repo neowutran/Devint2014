e1973896c112fc245c98ac5eda305d4270e2f909
Romain Guillot <rom1of0674@gmail.com>
2014-05-19 23:22:13 +0200

    no message

--- Project/music/music.js
+++ Project/music/music.js
@@ -2,6 +2,8 @@ var music_list = [
 	 {"file":"olive_et_tom.ogg"} , 
 {"file":"pokemon_generique.mp3"} ,
 {"file":"simpsons.mp3"} ,
+{"file":"tetris.ogg"} 
+{"file":"remix_cuir_moustache.mp3"} 
 	{"file":"test/InShape_-_Engine.ogg"}
 
 ];
